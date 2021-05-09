bb5d6818cd2629bce1ba771f3dfa1e15c40327ce
dosabalint <dosa.balint@gmail.com>
2021-05-09 14:28:31 +0200

    02-01 fix

--- 02-rtos-thread-01/main.cpp
+++ 02-rtos-thread-01/main.cpp
@@ -4,18 +4,34 @@
  */
 
 #include "mbed.h"
+#include "rtos.h"
 
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE 5000ms
+Thread thread1(osPriorityNormal, 1024);
+Thread thread2(osPriorityNormal, 1024);
 
-int main()
-{
-    // Initialise the digital pin LED1 as an output
-    DigitalOut led(LED1);
+void led1_thread() {
+  while (true) {
+    led1 = !led1;
+    ThisThread::sleep_for(750ms);
+  }
+}
 
-    while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
-    }
+void led2_thread() {
+  while (true) {
+    led2 = !led2;
+    ThisThread::sleep_for(500ms);
+  }
+}
+
+int main() {
+  thread1.start(led1_thread);
+  thread2.start(led2_thread);
+  while (true) {
+    led3 = !led3;
+    ThisThread::sleep_for(250ms);
+  }
 }
